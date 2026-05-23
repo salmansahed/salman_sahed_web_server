cec99527ace188c242be83f026f76f49ffe4adab
salmansahed <salmansahed10@gmail.com>
2026-05-23 19:51:02 +0600

    portfolio server

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // await client.connect();
+    const db = client.db("Salman_Sahed");
+    const projectCollection = db.collection("projects");
+
+    app.get("/projects", async (req, res) => {
+      const cursor = projectCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
