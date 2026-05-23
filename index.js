56ff2c77a9dff75fd2f72df1072f5562b4797452
salmansahed <salmansahed10@gmail.com>
2026-05-24 01:53:50 +0600

    add contact form submission API

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
+app.use(cors());
+app.use(express.json());
+
 const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
@@ -20,21 +23,32 @@ async function run() {
     // await client.connect();
     const db = client.db("Salman_Sahed");
     const projectCollection = db.collection("projects");
+    const contactsCollection = db.collection("contact-form");
 
+    // Home Route
     app.get("/", (req, res) => {
       res.send("Salman  Sahed Server API");
     });
 
+    // Get Project Data
     app.get("/projects", async (req, res) => {
       const cursor = projectCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // Get Single Project Data
     app.get("/projects/:id", async (req, res) => {
       const { id } = req.params;
       const query = { _id: new ObjectId(id) };
       const result = await projectCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Post Contact Form Data
+    app.post("/contact-form", async (req, res) => {
+      const data = req.body;
+      const result = await contactsCollection.insertOne(data);
       res.send(result);
     });
 
